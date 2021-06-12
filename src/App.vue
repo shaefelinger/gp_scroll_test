e42392231145db18e9615c096b7155ac8e65ea22
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-06-12 19:15:46 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    Hello
+    <ShowComments v-for="item in items" v-bind="item" :key="item.id" />
+    <TheObserver @intersect="Intersected" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import ShowComments from './components/ShowComments.vue';
+import TheObserver from './components/TheObserver.vue';
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: 'App',
+  components: { TheObserver, ShowComments },
+  data() {
+    return {
+      items: [],
+      page: 1,
+    };
+  },
+  methods: {
+    async Intersected() {
+      console.log('insersect');
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/comments?_page=${this.page}`
+      );
+      const items = await res.json();
+      this.items = [...this.items, ...items];
+      this.page++;
+      console.log(this.page);
+    },
+    pageScroll() {
+      window.scrollBy(0, 1);
+    },
+  },
+  mounted() {
+    this.timerId = setInterval(() => this.pageScroll(), 22);
+  },
+  destroyed() {
+    clearInterval(this.timerId);
   },
 };
 </script>
---
+++ src/components/ShowComments.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="comments">
+    <h1>{{ name }}</h1>
+    <h2>{{ id }}</h2>
+    <p>{{ body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowComments',
+  props: {
+    postId: Number,
+    id: Number,
+    name: String,
+    email: String,
+    body: String,
+  },
+};
+</script>
+
+<style></style>
